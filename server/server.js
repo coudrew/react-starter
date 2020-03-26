9438799e6a6aceaf46a6fdfc6799a3a85cfd83d2
coudrew <couture.andrew@gmail.com>
2020-03-26 01:04:02 +0000

    server up

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const hbs = exphbs.create();
+const PORT = 3030;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static('assets'));
+app.get('/', (req, res) => {
+	const baseAssetUrl = `${req.protocol}://${req.get('host')}`;
+	res.render('home', {
+		layout: false,
+		reactSrc: `${baseAssetUrl}/js/ui.bundle.js`
+	});
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
